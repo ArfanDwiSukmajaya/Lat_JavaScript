d95dc8464d726eacbaac0bf0d7f573c7d6eeb9e6
Arfan Dwi Sukmajaya <jayaarfan92@gmail.com>
2020-08-25 00:57:50 +0700

    membuat file reviewObject

---
+++ Dasar-DasarClass/reviewObject.js
@@ -0,0 +1,17 @@
+const user = {
+  name: 'arfan',
+  usia: 19
+}
+console.log(user.usia);
+
+
+
+// Object dan Function
+const mhs = {
+  name: 'Dwi',
+  usia: 20,
+  salam: () => {
+    console.log('Halo');
+  }
+};
+mhs.salam();
